2078ae086ee842a700b307714fa0cd28fe4f97bb
findstr <findstr@sina.com>
2015-07-04 13:13:10 +0800

    add simpledb(memory cache)

--- main.lua
+++ main.lua
@@ -1,32 +1,32 @@
 local server = require("server")
 local crypt = require("crypt")
 local json = require("json")
+local db = require("simpledb")
+local usrmgr = require("usrmgr")
+
 local CMD = {}
 
 local randomkey
-local usr
-local pwd
-
 
 function CMD.register(fd, data) 
-        usr = data.user
-        pwd = data.pwd
-        server.send(fd, "I receive it" .. str)
+        usrmgr.reg(data.user, data.pwd)
+        server.send(fd,  '{"cmd":"auth1", "response":"200"}\r\n\r')
 end
 
 function CMD.auth1(fd, data) 
         local res
         randomkey = crypt.randomkey()
-        res = '{"cmd":"auth1", "response":"' .. randomkey .. '"}'
+        res = '{"cmd":"auth1", "response":"' .. randomkey .. '"}\n'
         server.send(fd, res)
 end
 
 function CMD.auth2(fd, data)
+        local pwd = usrmgr.getpwd(data.usr)
         local hash = crypt.sha1(randomkey .. pwd);
         if hasn == data.pwd then
-                res = '{"cmd":"auth2", "response":"' .. 200 .. '"}'
+                res = '{"cmd":"auth2", "response":"' .. 200 .. '"}\n'
         else
-                res = '{"cmd":"auth2", "response":"' .. 403 .. '"}'
+                res = '{"cmd":"auth2", "response":"' .. 403 .. '"}\n'
         end
         
         server.send(fd, res)
@@ -46,12 +46,14 @@ print("hello lua")
 
 print("server", server)
 ]]--
+
 while true do
         local fd, data = server.pull()
         if (data) then
                 print("receive data:", data)
                 local t = json.decode(data)
-                assert(CMD[t.cmd])(fd, t, data)
+                assert(CMD[t.cmd])(fd, t)
                 --server.send(fd, "i have receive:" .. data)
         end
 end
+
---
+++ server-src/event.h
@@ -0,0 +1,6 @@
+#ifndef _EVENT_H
+#define _EVENT_H
+
+
+#endif
+
