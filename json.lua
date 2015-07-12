22a85566fdc04d18a7ec0232c27c264167d25ff4
findstr <findstr@sina.com>
2015-07-12 12:45:49 +0800

    add a stupid json.encode

--- json.lua
+++ json.lua
@@ -6,12 +6,33 @@ function json.decode(str)
 
         j = "local t =" .. j .."; return t"
  
-        print(j)
-
         t = load(j)()
 
         return t;
 end
 
+
+function json.encode(tbl)
+        local l = tbl or {}
+        local sz
+
+        sz = '{'
+        for k, v in pairs(tbl) do
+                sz = sz ..'"' .. k .. '"' .. ":" .. '"' .. v .. '"' .. ','
+        end
+
+        sz = sz .. '}\r\n\r'
+        
+        return sz
+end
+
+------------test--------
+--[[ 
+local tbl = {cmd="fdas", uid = "dafas"}
+
+local sz = json.encode(tbl)
+
+print(sz)
+]]--
 
 return json
