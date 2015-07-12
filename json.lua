25c264a520c5cf0d1ef2ade0c63fa20a2f593a88
findstr <findstr@sina.com>
2015-07-12 14:43:40 +0800

    add simple json array support

--- json.lua
+++ json.lua
@@ -11,28 +11,46 @@ function json.decode(str)
         return t;
 end
 
+local encode_tbl, encode_array
 
-function json.encode(tbl)
+function encode_array(arr)
+        local sz = ""
+        for _, v in ipairs(arr) do
+                assert(type(v) == "table")
+                sz = sz .. encode_tbl(v) .. ','
+        end
+
+        return sz
+end
+
+function encode_tbl(tbl)
         local l = tbl or {}
         local sz
 
         sz = '{'
         for k, v in pairs(tbl) do
-                sz = sz ..'"' .. k .. '"' .. ":" .. '"' .. v .. '"' .. ','
+                if (type(v) == "table") then
+                        sz = sz .. k .. ':' .. '[' ..  encode_array(v) .. '],'
+                else
+                        sz = sz ..'"' .. k .. '"' .. ":" .. '"' .. v .. '"' .. ','
+                end
         end
 
-        sz = sz .. '}\r\n\r'
-        
+        sz = sz .. '}'
+
         return sz
 end
 
+function json.encode(tbl)
+        local sz = encode_tbl(tbl)
+        return (sz .. '\r\n\r')
+end
+
 ------------test--------
---[[ 
-local tbl = {cmd="fdas", uid = "dafas"}
+local tbl = {room={{cmd="auth1", uid = "dafas"},
+                {cmd="auth2", sid = "123"}}}
 
 local sz = json.encode(tbl)
-
 print(sz)
-]]--
 
 return json
