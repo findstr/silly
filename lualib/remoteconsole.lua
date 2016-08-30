cb715d16c9fc376031771a677c1af1eb50c07ff8
findstr <findstr@sina.com>
2016-08-30 19:46:05 +0800

    rewrite remoteconsole

--- lualib/remoteconsole.lua
+++ lualib/remoteconsole.lua
@@ -6,6 +6,10 @@ local CMD = {}
 function CMD.exit(fd, line)
 end
 
+function CMD.ping(fd, line)
+        socket.write(fd, "pong\n")
+end
+
 function CMD.memstatus(fd, line)
         local sz = core.memstatus()
         socket.write(fd, "Memory Used:" .. sz .. "\n")
@@ -16,28 +20,36 @@ function CMD.msgstatus(fd, line)
         socket.write(fd, "Unprocessed Message Count:" .. sz .. "\n")
 end
 
-socket.listen("127.0.0.1@2323", function(fd, addr)
-        print(fd, "from", addr)
-        while true do
-                local res = socket.readline(fd)
-                if not res then
-                        break
-                end
-                local cmd = res:match("[%g]+")
-                if not cmd then
-                        print("incorrect format:", res)
-                elseif cmd == "quit" then
-                        socket.close(fd)
-                        break
-                else
-                        local func = CMD[cmd];
-                        if not func then
-                                print("unsupport command:" .. cmd)
+local function console(config)
+        socket.listen(config.addr, function(fd, addr)
+                print(fd, "from", addr)
+                while true do
+                        local res = socket.readline(fd)
+                        if not res then
+                                break
+                        end
+                        local cmd = res:match("[%g]+")
+                        if not cmd then
+                                print("incorrect format:", res)
+                        elseif cmd == "quit" then
+                                socket.close(fd)
+                                break
                         else
-                                func(fd, res)
+                                local func = CMD[cmd];
+                                if not func then
+                                        func = config[cmd]
+                                end
+                                if not func then
+                                        print("unsupport command:" .. cmd)
+                                else
+                                        func(fd, res)
+                                end
                         end
                 end
-        end
-        print(addr, "disconnected")
+                print(addr, "disconnected")
 end)
+
+end
+
+return console
 
