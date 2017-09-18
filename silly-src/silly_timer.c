64250da19dc3f25014ea211f2ba04aee1e0e97d9
findstr <findstr@sina.com>
2017-09-18 20:43:55 +0800

    macosx use SYSTEM_CLOCK as MONOTONIC

--- silly-src/silly_timer.c
+++ silly-src/silly_timer.c
@@ -165,7 +165,7 @@ ticktime()
 #ifdef __macosx__
 	clock_serv_t cclock;
 	mach_timespec_t mts;
-	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+	host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
 	clock_get_time(cclock, &mts);
 	mach_port_deallocate(mach_task_self(), cclock);
 	ms = (uint64_t)mts.tv_sec * 1000 / RESOLUTION;
