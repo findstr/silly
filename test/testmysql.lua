50cdc2d5043097ccdac997072b734c169d61586d
重归混沌 <findstr@sina.com>
2020-10-28 10:26:51 +0800

    add ci action

--- test/testmysql.lua
+++ test/testmysql.lua
@@ -1,15 +1,15 @@
 local core = require "sys.core"
 local mysql = require "sys.db.mysql"
 local testaux = require "testaux"
+local json = require "sys.json"
 
 return function()
-	local db = mysql.create {
+	local db = mysql.connect{
 		addr ="127.0.0.1:3306",
 		user="root",
 		password="root",
 	}
-	db:connect()
 	local status, res = db:query("select 0;")
-	testaux.asserteq(res[1]["0"], "0", "select 0;")
+	testaux.asserteq(res[1]["0"], 0, "select 0;")
 end
 
