1f9d173406f5d4615296618258a5d96e567d5cc6
findstr <findstr@sina.com>
2023-06-18 07:27:49 +0800

    remove cluster library(except rpc.lua/msg.lua)
    we can have a more flexible distributed solution

--- examples/start.sh
+++ examples/start.sh
@@ -1,17 +1,4 @@
-#!/bin/sh
-
-cluster() {
-	echo "start master"
-	./silly examples/cluster/master/start.conf --master="127.0.0.1:9000" &
-	sleep 1
-	./silly examples/cluster/gate/start.conf --master="127.0.0.1:9000" --gate="127.0.0.1:9001" --gate_listen="127.0.0.1:9002" &
-	./silly examples/cluster/gate/start.conf --master="127.0.0.1:9000" --gate="127.0.0.1:9003" --gate_listen="127.0.0.1:9004" &
-	./silly examples/cluster/auth/start.conf --master="127.0.0.1:9000" --auth="127.0.0.1:9005" --auth_listen="127.0.0.1:9006" &
-	./silly examples/cluster/role/start.conf --master="127.0.0.1:9000" --role="127.0.0.1:9007" &
-	./silly examples/cluster/role/start.conf --master="127.0.0.1:9000" --role="127.0.0.1:9008" &
-	sleep 3 #wait for stable
-	wait
-}
+#!/bin/bash
 
 module() {
 	./silly --lualib_path="lualib/?.lua;examples/?.lua" --lualib_cpath="luaclib/?.so" --bootstrap="examples/$1.lua"
@@ -23,14 +10,10 @@ all() {
 		echo "---------------$i-------------------"
 		timeout 5 ./silly --lualib_path="lualib/?.lua;examples/?.lua" --lualib_cpath="luaclib/?.so" --bootstrap="$i"
 	done
-	cluster
-	sleep 30
 	exit 0
 }
 
 case $1 in
-	"cluster")
-		cluster;;
 	"")
 		all;;
 	*)
