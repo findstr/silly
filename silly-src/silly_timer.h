6e956c4cb67b185d8693b1d4b1d3892426570eb8
findstr <findstr@sina.com>
2023-08-08 13:58:26 +0800

    timer: support cancel and redesign node cache

--- silly-src/silly_timer.h
+++ silly-src/silly_timer.h
@@ -6,7 +6,8 @@
 void silly_timer_init();
 void silly_timer_exit();
 void silly_timer_update();
-uint32_t silly_timer_timeout(uint32_t expire);
+uint64_t silly_timer_timeout(uint32_t expire, uint32_t ud);
+int silly_timer_cancel(uint64_t session, uint32_t *ud);
 uint64_t silly_timer_now();
 time_t silly_timer_nowsec();
 uint64_t silly_timer_monotonic();
