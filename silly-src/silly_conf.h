5ee547501b131142787df1d101e3835d8410580b
findstr <findstr@sina.com>
2025-04-29 21:22:54 +0800

    allow custom macros in silly_config.h via MYCFLAGS

--- silly-src/silly_conf.h
+++ silly-src/silly_conf.h
@@ -1,6 +1,20 @@
 #ifndef _SILLY_CONF_H
 #define _SILLY_CONF_H
 
+#define STR(s) __STR(s)
+#define __STR(s) #s
+
+#define TRACE_WORKER_ID (0)
+#define TRACE_TIMER_ID (1)
+#define TRACE_SOCKET_ID (2)
+#define TRACE_MONITOR_ID (3)
+
+#ifdef __WIN32
+#define LUA_LIB_SUFFIX ".dll"
+#else
+#define LUA_LIB_SUFFIX ".so"
+#endif
+
 //platform related
 
 #ifdef __linux__
@@ -22,27 +36,32 @@
 #endif
 
 //(1 << 16) = 65536
+#ifndef SOCKET_MAX_EXP
 #define SOCKET_MAX_EXP (16)
+#endif
+
+#ifndef TIMER_RESOLUTION
 #define TIMER_RESOLUTION (10)                     //ms
+#endif
+
+#ifndef TIMER_ACCURACY
 #define TIMER_ACCURACY (50)                       //ms
+#endif
+
+#ifndef TIMER_DELAY_WARNING
 #define TIMER_DELAY_WARNING (10 * TIMER_ACCURACY) //ms
+#endif
+
+#ifndef MONITOR_MSG_SLOW_TIME
 #define MONITOR_MSG_SLOW_TIME (1000)              //ms
+#endif
 
-#define STR(s) __STR(s)
-#define __STR(s) #s
-
+#ifndef LOG_BUF_SIZE
 #define LOG_BUF_SIZE (4 * 1024)
-#define LOG_ENABLE_FILE_LINE 1
+#endif
 
-#define TRACE_WORKER_ID (0)
-#define TRACE_TIMER_ID (1)
-#define TRACE_SOCKET_ID (2)
-#define TRACE_MONITOR_ID (3)
-
-#ifdef __WIN32
-#define LUA_LIB_SUFFIX ".dll"
-#else
-#define LUA_LIB_SUFFIX ".so"
+#ifndef LOG_DISABLE_FILE_LINE
+#define LOG_ENABLE_FILE_LINE
 #endif
 
 #endif
